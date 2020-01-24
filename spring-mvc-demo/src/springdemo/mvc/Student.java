db8a70d0e129c6337c8d5c7acc146082fb460bf7
novakvova <novakvova@gmail.com>
2020-01-24 17:55:39 +0200

    Add radion button in form spring

--- spring-mvc-demo/src/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/springdemo/mvc/Student.java
@@ -7,6 +7,8 @@ public class Student {
 	private String lastName;
 
 	private String country;
+	
+	private String favoriteLanguage;
 
 	private LinkedHashMap<String, String> countryOptions;
 
@@ -47,5 +49,13 @@ public class Student {
 
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
+	}
+	
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
 	}
 }
